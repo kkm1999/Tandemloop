7c1a453bd5f99b6d5309df03d87406eeee761d3c
Kishor Mandal <65722722+kkm1999@users.noreply.github.com>
2025-11-28 23:11:07 +0530

    Create Program-2.cpp

---
+++ Program-2.cpp
@@ -0,0 +1,13 @@
+// First ‘a’ Odd Numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    for (int i = 0; i < a; i++)
+        cout << (2 * i + 1) << " ";
+
+    return 0;
+}
